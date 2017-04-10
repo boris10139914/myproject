e4589f2635c7c8dca24982f9b995343edcb448dd
黃元宏 <boris556655@gmail.com>
2017-04-10 06:21:53 -0700

    練習:發票中獎

--- src/oo/homework/Invoice.java
+++ src/oo/homework/Invoice.java
@@ -9,20 +9,31 @@ public class Invoice {
 		// 作業: 照片 把4個號碼用arraylist處理然後用迴圈比對是否中獎
 		String q = "331";
 		// ---------012345678 第幾格
-		String n = "123456331";
-		System.out.println(n.length());
+		//String n = "123456331";
+		//System.out.println(n.length());
 		// String sub =n.substring(5);//5代表取第5個後面的
-		String sub = n.substring(n.length() - 3);
-		System.out.println(sub);
+	
 		ArrayList<String> a = new ArrayList();
 		Scanner s = new Scanner(System.in);
+		boolean o = true;
 		a.add("331");
 		a.add("821");
 		a.add("886");
 		a.add("554");
-		while (a.size() == 4) {
-			System.out.println("請輸入發票號碼:");
+		while (o==true) {
+			System.out.println("請輸入8位發票號碼:");
 			String invoice = s.nextLine();
+			System.out.println("您輸入的發票:"+invoice+"   中獎末三碼"+ q);
+			String sub = invoice.substring(invoice.length() - 3);
+			System.out.println(sub);
+			int i = Integer.parseInt(sub);
+			int j = Integer.parseInt(q);
+			System.out.println(i);
+			System.out.println(j);
+			if(i==j){
+			System.out.println("恭喜您中獎");
+			break;
+			}
 //自己思考
 		}
 
