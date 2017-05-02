f1eab4b8fbc6714b1b78d420225735ec59ca621e
黃元宏 <boris556655@gmail.com>
2017-05-02 00:02:28 -0700

    真是太趕了

---
+++ src/oo/stati/GraduateStudent.java
@@ -0,0 +1,10 @@
+package oo.stati;
+
+public class GraduateStudent extends Student {
+	int thisis;
+	public	GraduateStudent(int english, int math, int chinese,int thisis){
+		this.english = english;
+		this.math = math;
+		this.chinese = chinese;
+}
+}
