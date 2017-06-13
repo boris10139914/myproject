84e7dd97b90ec2f226e750848cbd9d68d6f6b95f
黃元宏 <boris556655@gmail.com>
2017-06-13 15:33:25 +0800

    2017/6/13 hide the implementation 練習與教學

--- src/oo/test/MemberTester.java
+++ src/oo/test/MemberTester.java
@@ -3,9 +3,13 @@ package oo.test;
 public class MemberTester {
 
 	public static void main(String[] args) {
-		Member member = new Member();
+		Member member = new Member();//印建構子
 		member.setAge(-5);
 		System.out.println(member.getAge());
+		
+		//Phone Member
+		PhoneMember member2 = new PhoneMember();//印建構子
+		
 	}
 
 }
--- src/oo/test/PhoneMember.java
+++ src/oo/test/PhoneMember.java
@@ -3,5 +3,6 @@ package oo.test;
 public class PhoneMember extends Member {
 	String phone;
 	boolean verified = false;
-
+//沒有建構子會補一個java會自動補一個建構子,裡面有super();
+//呼叫父類別的建構子,也就是說會再印一次
 }
